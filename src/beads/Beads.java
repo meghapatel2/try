5065bb2a658b4a1a3f51a5ed1779c9ffcb5038f1
meghapatel2 <50331958+meghapatel2@users.noreply.github.com>
2019-05-21 13:46:17 -0400

    comment val

--- src/beads/Beads.java
+++ src/beads/Beads.java
@@ -14,7 +14,7 @@ public class Beads {
 
     public String color;
     public char letter;
-    public int val;
+   // public int val;
         
     /**
      * @param args the command line arguments
